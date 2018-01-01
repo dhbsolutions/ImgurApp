9757a3821b246f7840defacd95bf4397067bda89
Dave Butler <dhbsolutions@verizon.net>
2018-01-01 18:52:18 -0500

    Initial Revision

--- Pods/Target Support Files/Pods-ImgurApp/Pods-ImgurApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ImgurApp : NSObject
-@end
-@implementation PodsDummy_Pods_ImgurApp
-@end
--- Pods/Target Support Files/Pods-ImgurAppTests/Pods-ImgurAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ImgurAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_ImgurAppTests
-@end
