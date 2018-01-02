2dd0925ee68f0dfb1d8e73452c00e5431e8e82cc
Dave Butler <dhbsolutions@verizon.net>
2018-01-01 23:52:28 -0500

    Added simple Unit Test

--- ImgurAppTests/ImgurAppTests.swift
+++ ImgurAppTests/ImgurAppTests.swift
@@ -21,9 +21,34 @@ class ImgurAppTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testImgurWebsite() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // TEST THAT THE IMGUR WEBSITE IS UP AND THAT WE CAN SUCCESSFULLY HIT THE MAIN PAGE
+        let url = URL(string: "http://www.imgur.com/")!
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { (data, response, error) in
+            XCTAssertNotNil(data, "data should not be nil")
+            XCTAssertNil(error, "error should be nil")
+            
+            if let response = response as? HTTPURLResponse,
+                let responseURL = response.url,
+                let mimeType = response.mimeType
+            {
+                XCTAssertEqual(responseURL.absoluteString, url.absoluteString, "HTTP response URL should be equal to original URL")
+                XCTAssertEqual(response.statusCode, 200, "HTTP response status code should be 200")
+                XCTAssertEqual(mimeType, "text/html", "HTTP response content type should be text/html")
+            } else {
+                XCTFail("Responses from http://www.imgur.com/ not as expected, check that site is available")
+            }
+            
+        }
+        
+        task.resume()
+
+        
+        
     }
     
     func testPerformanceExample() {
